2c264bd837c02abf88a45d0fa5e391f9b61ca227
Zenobia <72229654+charlzen@users.noreply.github.com>
2021-02-03 13:56:08 -0800

    index edits

--- contact.php
+++ contact.php
@@ -1,14 +1,37 @@
-<?php
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
-    $message = $_POST['message'];
+<!DOCTYPE html>
+<html>
+  <head>
+    <html lang = "en">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main_style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap" rel="stylesheet">
+    <title>Paul Chaat Smith | Contact</title>
+  </head>
 
-    $mailTo = "z.ch2012@yahoo.com";
-    $headers = "From: " .$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+<!-- Start of Webpage Content-->
+<!-- Use SVGs for Webpage Shapes-->
+<body>
+    <header>
+        <h1>Paul Chaat Smith</h1>
+        <p>Author | Essayist | Curator</p>
+    </header>
+    <nav>
+      <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="books.html">Books</a></li>
+        <li><a href="texts.html">Texts</a></li>
+        <li><a href="exhibitions.html">Exhibitions</a></li>
+        <li><a href="read.html">Featured Read</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+    </nav>
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
-}
+<p>Contact Me</p>
+<form class = "contact-form" action = "contact.php" method = "post">
+  <input type = "text" name = "name" placeholder = "Name">
+  <input type = "text" name = "mail" placeholder = "Your E-mail">
+  <input type = "text" name = "subject" placeholder = "Subject">
+  <textarea name = "message" placeholder = "Message"></textarea>
+  <button type = "submit" name = "submit">Submit</button>
+</form>
